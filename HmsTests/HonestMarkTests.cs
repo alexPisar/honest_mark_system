b125fdba95a5f19cb7f24ef3b22e4d80f2db4736
alexPisar <lesha1995@list.ru>
2022-03-24 12:42:01 +1000

    Добавил тест получения статуса в Честном знаке

---
+++ HmsTests/HonestMarkTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cryptography.WinApi;
+using WebSystems.WebClients;
+
+namespace HmsTests
+{
+    [TestClass]
+    public class HonestMarkTests
+    {
+        [TestMethod]
+        public void GetStatusTest()
+        {
+            var crypto = new WinApiCryptWrapper();
+            var cert = crypto.GetCertificateWithPrivateKey("1AE6FE62C7DEE1C4CA5AAAF9A9B33AFA95640753", false);
+            HonestMarkClient.GetInstance().Authorization(cert);
+
+            var status = HonestMarkClient.GetInstance().GetEdoDocumentProcessInfo("DP_PRANNUL_2AE97D403BD-870F-4CD7-87EE-1F384836B637_2BM-2538150215-253801001-201605120119377915440_20220310_b35583e8-2a79-480e-bbb4-5f62d2630595");
+        }
+    }
+}
