204b699860d8fea1ea2975d71379039b5f83fcb9
alexPisar <support.db@wera-rus.com>
2023-02-09 16:50:13 +1000

    Добавил новую таблицу грузополучателей в базу данных

---
+++ DataContextManagementUnit/DataAccess/Mappings/RefUserByEdoConsignorConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace DataContextManagementUnit.DataAccess.Contexts.Abt.Mapping
+{
+    public partial class RefUserByEdoConsignorConfiguration : EntityTypeConfiguration<RefUserByEdoConsignor>
+    {
+        public RefUserByEdoConsignorConfiguration()
+        {
+            this
+                .HasKey(r => new { r.IdCustomer, r.UserName })
+                .ToTable("REF_USERS_BY_EDO_CONSIGNORS", "EDI");
+
+            this
+                .Property(r => r.IdCustomer)
+                .HasColumnName("ID_CUSTOMER");
+
+            this
+                .Property(r => r.UserName)
+                .HasColumnName("USER_NAME")
+                .HasMaxLength(100);
+
+            OnCreated();
+        }
+
+        partial void OnCreated();
+    }
+}
